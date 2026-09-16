d1947c45208c2ebb8cb6e97a8b4015987cbe0fbe
Wihred <Bonikmaikl@gmail.com>
2026-09-16 18:58:35 +0700

    cleanup: disable legacy watchdog during install

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,6 @@
 @echo off
 title BlueAudioSwitch - Install
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'BlueAudioSwitchSpeakerFallback' -ErrorAction SilentlyContinue; Get-CimInstance Win32_Process -ErrorAction SilentlyContinue ^| Where-Object { $_.CommandLine -match '[\\/]SpeakerFallback\.ps1' } ^| ForEach-Object { Invoke-CimMethod -InputObject $_ -MethodName Terminate -ErrorAction SilentlyContinue ^| Out-Null }"
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ApplyFallbackPatch.ps1" -Target "%~dp0BlueAudioSwitch.ps1"
 if errorlevel 1 (
   echo.
