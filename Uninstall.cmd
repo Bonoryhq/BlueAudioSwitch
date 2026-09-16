b8b232ca237d772824737f092efe9cae20e8b9cf
Wihred <Bonikmaikl@gmail.com>
2026-09-16 18:43:17 +0700

    fix: uninstall HS5 speaker fallback watchdog

--- Uninstall.cmd
+++ Uninstall.cmd
@@ -1,5 +1,6 @@
 @echo off
 title BlueAudioSwitch - Uninstall
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0SpeakerFallback.ps1" -Uninstall
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0BlueAudioSwitch.ps1" -Uninstall
 echo.
 echo Uninstall command completed.
